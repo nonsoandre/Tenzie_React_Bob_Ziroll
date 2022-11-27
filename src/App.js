e94f3e215310f036ca69a815e617783a89b1373b
nonsoandre <nonsoandre@gmail.com>
2022-11-27 17:34:59 +0100

    Dice game setup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
+    return (
+      <main>
+          <h1 className="title">Tenzies</h1>
+          <p className="instructions">Roll until all dice are the same. 
+          Click each die to freeze it at its current value between rolls.</p>
+          <div className="dice-container">
+              {/* {diceElements} */}
+          </div>
+          <button 
+              className="roll-dice" 
+              // onClick={rollDice}
+          >
+              {/* {tenzies ? "New Game" : "Roll"} */} Roll
+          </button>
+      </main>
   );
 }
 
