315e645505e761a4367a6aa405d30963d2494866
nonsoandre <nonsoandre@gmail.com>
2022-11-27 18:27:36 +0100

    Changing Dice State
    - set function to create random arrays btwn 1-6 in 10 places
    
    - assign the array created to react state
    
    - write function that displays these values
    
    - set the dice to always call the function such that the react state is refreshed.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Die from './Die';
+
+
 
 function App() {
+  //react state to ttrack dices
+  const [dice, setDices] = useState(allNewDice())
+
+    // function that returns an array of 10 random number
+    function allNewDice (){
+      const arrayElement = [];
+      for (let i = 0; i < 10; i++){
+          arrayElement.push(Math.ceil(Math.random(i) * 6));
+        }
+        return arrayElement;
+    }
+
+    
+    const diceElements = dice.map( dies => <Die value={dies} />)
+
+    
+    function rollDice(){
+      setDices(allNewDice());
+    }
+
     return (
       <main>
           <h1 className="title">Tenzies</h1>
           <p className="instructions">Roll until all dice are the same. 
           Click each die to freeze it at its current value between rolls.</p>
           <div className="dice-container">
-              {/* {diceElements} */}
+              {diceElements}  
           </div>
           <button 
               className="roll-dice" 
-              // onClick={rollDice}
+              onClick={rollDice}
           >
               {/* {tenzies ? "New Game" : "Roll"} */} Roll
           </button>
